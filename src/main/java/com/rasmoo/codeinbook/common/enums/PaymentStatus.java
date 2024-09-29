3fb55852763334bec4978f543f77a201f1d02f83
Felipe Alves <felipe.barbosa@ext.valid.com>
2024-09-29 00:43:41 -0300

    Fix bootstrap variable

--- src/main/java/com/rasmoo/codeinbook/common/enums/PaymentStatus.java
+++ src/main/java/com/rasmoo/codeinbook/common/enums/PaymentStatus.java
@@ -5,5 +5,4 @@ public enum PaymentStatus {
     APPROVED,
     DENIED
 
-
 }
--- src/main/java/com/rasmoo/codeinbook/infrastructure/configuration/KafkaProducerConfiguration.java
+++ src/main/java/com/rasmoo/codeinbook/infrastructure/configuration/KafkaProducerConfiguration.java
@@ -18,7 +18,7 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfiguration {
 
-    @Value("${spring.kafka.bootstrap-servers:localhost:9092")
+    @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
     private String bootstrapAddress;
 
     @Bean
